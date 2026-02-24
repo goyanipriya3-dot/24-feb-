998244b7129d4904557f625bd7e3c3f13456bf70
goyanipriya3-dot <goyanipriya3@gmail.com>
2026-02-24 12:28:36 +0000

    24 feb

---
+++ Arithmetic Operator.c
@@ -0,0 +1,18 @@
+
+Arithmetic Operator: Use For Calculation 
++,-,*,/,%
+% : Modulo, Modules and many more ..
+
+
+#include (stdio.h)
+
+
+int main() {
+     int num_1=10,num_2=2
+     printf("\nsum is %d\n," num_1+num_2);
+     printf("\nsub is %d\n," num_1-num_2);
+     printf("\nmul is %d\n," num_1-*um_2);
+     printf("\ndiv is %d\n," num_1/num_2);
+     printf("\nmod is %d\n," num_1%num_2);
+     return 0;
+}
